158024242d852b7724a0b0ac511ccf393e91e1b5
Anna <ankatkav@gmail.com>
2020-03-20 21:30:14 +0200

    Fixed multidigit numbers

--- src/CalculatorParser.java
+++ src/CalculatorParser.java
@@ -65,7 +65,7 @@ public class CalculatorParser {
             int returnValue = expr();
             consume(')');
             return returnValue;
-        }else if(lookaheadToken < 0 || lookaheadToken > 9){
+        }else if(lookaheadToken >= '0' && lookaheadToken <= '9'){
             return num();
         }else {
             throw new ParseError();
@@ -75,12 +75,35 @@ public class CalculatorParser {
     private int num() throws IOException, ParseError{
         int digit = evalDigit(lookaheadToken);
         consume(lookaheadToken);
+        digit = getFullDigit(String.valueOf(digit));
         return digit;
+    }
+
+    private int getFullDigit(String number) throws IOException, ParseError{
+        if((lookaheadToken >= '0' && lookaheadToken <= '9')) {
+            int newDigit = evalDigit(lookaheadToken);
+            number += newDigit;
+            consume(lookaheadToken);
+            return getFullDigit(number);
+        }else if(lookaheadToken == '(' || lookaheadToken == ')' || lookaheadToken == '+' || lookaheadToken == '-'
+                || lookaheadToken == '*' || lookaheadToken == '/' || lookaheadToken == -1 || lookaheadToken == '\n')
+            return Integer.parseInt(number);
+        else
+            throw new ParseError();
     }
 
     private int evalDigit(int digit){
         return digit - '0';
     }
+
+    /*        int fisrtNum = digit;
+        consume(digit);
+        int nextToken = lookaheadToken;
+        if((lookaheadToken >= '0' && lookaheadToken <= '9')){
+            String strDigit = String.valueOf(fisrtNum-'0') + String.valueOf(nextToken-'0');
+            digit = Integer.parseInt(strDigit + (evalDigit(nextToken)-'0'));
+            return digit;
+        }*/
 
     private void consume(int symbol) throws IOException, ParseError{
         if (lookaheadToken != symbol){
